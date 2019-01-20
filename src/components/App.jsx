bb79dabaea33d432bf6e51cb2d2b94b5f2d94215
Jose Mario Gutierrez <jogutierrez@H7689.ad.hugeinc.com>
2019-01-20 13:20:50 -0500

    Updated component extension from js to jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./styles/App.css";
+
+class App extends Component {
+  render() {
+    return <h1> React Pokedex </h1>;
+  }
+}
+
+export default App;
